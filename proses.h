14e35fe0da7996e8ec41192e92476dd654eb7b1a
BrilianPermataZulfa <101861091+BrilianPermataZulfa@users.noreply.github.com>
2022-06-24 15:46:16 +0700

    Update proses.h

--- proses.h
+++ proses.h
@@ -1,7 +1,22 @@
 void Pointer::Proses(){
-    nim = new int[5];
-    nama = new string[5];
-    for(int i=0; i<5; i++){
+    nim = new int[data];
+    nama = new string[data];
+  	int temp;
+    string temp_nama;
+	  for (int i=1;i<data;i++){
+		  for(int j=data-1;j>=1;j--){
+			  if(nimMhs[j]<nimMhs[j-1]){
+				  temp=nimMhs[j];
+				  nimMhs[j]=nimMhs[j-1];
+				  nimMhs[j-1]=temp;
+          
+				  temp_nama=namaMhs[j];
+				  namaMhs[j]=namaMhs[j-1];
+				  namaMhs[j-1]=temp_nama;
+			  }
+		  }
+	  }
+    for(int i=0; i<data; i++){
       *nim = nimMhs[i];
       nim += 1;
       *nama = namaMhs[i];
