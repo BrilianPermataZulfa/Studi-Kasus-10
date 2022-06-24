f18d187859f3e6b2e6fb28fed4d21ad7df4e9972
BrilianPermataZulfa <101861091+BrilianPermataZulfa@users.noreply.github.com>
2022-06-24 14:41:46 +0700

    Create class.h

---
+++ class.h
@@ -0,0 +1,13 @@
+using namespace std;
+class Pointer{
+  public:
+    void Input();
+    void Proses();
+    void Output();
+  private:
+    int data;
+    int *nim;
+    int nimMhs[5];
+    string *nama;
+    string namaMhs[5];
+};
