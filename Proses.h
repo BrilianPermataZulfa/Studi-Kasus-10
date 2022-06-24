452ec38796f888ec5a12507183220f369a723bf7
BrilianPermataZulfa <101861091+BrilianPermataZulfa@users.noreply.github.com>
2022-06-24 14:16:36 +0700

    Update Proses.h

--- Proses.h
+++ Proses.h
@@ -1,13 +1,10 @@
 void Pointer::Proses(){
-  	cout << "=========================================="<<endl;
-  	cout << "Proses menyimpan data ke alamat pointer..."<<endl;
-  	cout << "=========================================="<<endl;
     nim = new int[5];
     nama = new string[5];
     for(int i=0; i<5; i++){
       *nim = nimMhs[i];
       nim += 1;
       *nama = namaMhs[i];
-      nama+= 1;
+      nama += 1;
     }
   }
