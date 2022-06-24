d6d85a9471d5c602b859755516765dc134cd7753
BrilianPermataZulfa <101861091+BrilianPermataZulfa@users.noreply.github.com>
2022-06-24 14:41:15 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "library/Class.h"
+#include "library/Input.h"
+#include "library/Proses.h"
+#include "library/Output.h"
+int main(){
+  Pointer x;
+  x.Input();
+  x.Proses();
+  x.Output();
+  
+  return 0;
+}
