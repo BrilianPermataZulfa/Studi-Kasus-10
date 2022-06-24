9d46682f9d803429f743e50e8616728b660453f0
BrilianPermataZulfa <101861091+BrilianPermataZulfa@users.noreply.github.com>
2022-06-24 14:42:25 +0700

    Create input.h

---
+++ input.h
@@ -0,0 +1,17 @@
+void Pointer::Input(){
+    cout << "\n"<<endl;
+    cout << "   Jumlah Data Finalis -> ";
+    cin >> data;
+    cout << "\n\n=========================================="<<endl;
+    cout << "   Masukkan Data Finalis Parmatika"<<endl;
+    cout << "=========================================="<<endl;
+    cout << "\n"<<endl;
+    for (int i=0; i<data; i++){
+      cout << "Masukkan NIM : ";
+      cin >> nimMhs[i];
+      cin.ignore();
+      cout << "Masukkan Nama : ";
+      getline(cin,namaMhs[i]);
+      cout<<"\n"<<endl;
+    }
+  }
